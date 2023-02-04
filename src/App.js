4b458a9685111241c4683483782d938f8337b789
UI-Monika <109434700+UI-Monika@users.noreply.github.com>
2023-02-04 22:48:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import Leftside from  './component/leftside';
+import Rideside from  './component/rideside';
+import Middleside from  './component/middleside';
+
 
 function App() {
   return (
    
-   <>
+   <div className='main'>
+   <div className='Root__top-container'>
    <h1>hello world</h1>
    <Leftside/>
+   <Rideside/>
+   <Middleside/>
    
-
-   </>
+   </div>
+   </div>
+   
   );
 }
 
